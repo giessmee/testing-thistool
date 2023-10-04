e10656bfb0440f05d3a5d97dfbdc870db12a6c30
giessmee <146904241+giessmee@users.noreply.github.com>
2023-10-04 15:23:48 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log ("console log is used to print any message that needs to be displayed to the user. ")
+console.log("this is a message from me to you")
+
+window.alert("Float in space?")
+
